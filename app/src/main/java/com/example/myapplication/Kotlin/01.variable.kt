0e690322b1648734e54b2ad430a5976102844e5d
tae77 <https://github.com/Jake-huen/Android_Study.git>
2021-03-09 17:50:32 +0900

    Android_Study(Kotlin)

---
+++ app/src/main/java/com/example/myapplication/Kotlin/01.variable.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication.Kotlin
+
+//01.Variable -> 변수
+//상자
+
+//변수 선언하는 방법
+// var/val 변수명 = 값
+
+var num = 10
+val hello = "hello"
+var point = 3.4
+
+val fix = 20
+
+fun main(args:Array<String>){
+
+    println(num)
+
+}
---
+++ app/src/main/java/com/example/myapplication/Kotlin/03.practice.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication.Kotlin.`03`
+
+var a = 1+2+3+4+5
+var b = "1"
+var c = b.toInt()//b를 int로 바꾼값이 저장됨
+var d = b.toFloat()//d에는 1.0이 있음
+
+var e = "John"
+var f = "My name is $e Nice to meet you"
+
+//Null
+//-존재하지 않는다
+
+//var abc : Int = null
+var abc1 : Int? = null//자료형 뒤에 ?를 붙이면 null을 가질 수 있는 자료형이 된다.
+
+var g = a+3
+fun main(array:Array<String>){
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(abc1)
+
+    println(g)
+}
